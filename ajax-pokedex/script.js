15c35649bb01ab2ae163fb20b4ab71bfc17aafa2
Greet Verduyn <greetverduyn@proximus.be>
2022-06-26 20:27:38 +0200

    put name, id, img in HTML

--- ajax-pokedex/script.js
+++ ajax-pokedex/script.js
@@ -2,10 +2,12 @@
 let reference= 'https://pokeapi.co/api/v2/pokemon/';
 const searchField = document.getElementById('search');
 
+
 async function getData(pokemon){
     const response = await fetch(reference + pokemon)
     var data = await response.json();
     return data;
+
 }
 
 
@@ -14,12 +16,18 @@ function searchPokemon(){
     getData(pokemon).then(response =>{
       let namePokemon = response.name;
       let idPokemon = response.id;
+      let imgPokemon = response.sprites.front_default;
         console.log(namePokemon);
         console.log(idPokemon);
+        console.log("img", imgPokemon);
+        console.log(response);
+        document.getElementById("namePokemon").innerHTML= namePokemon;
+        document.getElementById("idPokemon").innerHTML=` id ${idPokemon}`;
+        document.getElementById("imagePokemon").src= imgPokemon;
+//document.getElementById("image").appendChild("")
     } )
 
 
 }
-
 
 
