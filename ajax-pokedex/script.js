7e36edf968d123ceb850cd86fdd898ab4f7db637
Greet Verduyn <greetverduyn@proximus.be>
2022-06-30 20:50:32 +0200

    moves in console

--- ajax-pokedex/script.js
+++ ajax-pokedex/script.js
@@ -1,33 +1,82 @@
+'use strict';
+
+
+
 
 let reference= 'https://pokeapi.co/api/v2/pokemon/';
+let referenceMoves='';
+//let moves=[];
 const searchField = document.getElementById('search');
 
 
-async function getData(pokemon){
-    const response = await fetch(reference + pokemon)
-    var data = await response.json();
+async function getData(url){
+    const response = await fetch(url)
+    let data = await response.json();
     return data;
+//console.log(data)
+}
 
+function getMoves(moves){
+    if (moves.length >=3) {
+        for (let i = 0; i <= 3; i++) {
+            console.log(moves[i].move.name)
+        }
+    } else {
+        for (let i = 0; i < moves.length; i++) {
+            console.log(moves[i].move.name)
+        }
+    }
 }
 
 
-function searchPokemon(){
+
+
+function searchPokemon() {
     let pokemon = searchField.value;
-    getData(pokemon).then(response =>{
-      let namePokemon = response.name;
-      let idPokemon = response.id;
-      let imgPokemon = response.sprites.front_default;
-        console.log(namePokemon);
-        console.log(idPokemon);
-        console.log("img", imgPokemon);
-        console.log(response);
-        document.getElementById("namePokemon").innerHTML= namePokemon;
-        document.getElementById("idPokemon").innerHTML=` id ${idPokemon}`;
-        document.getElementById("imagePokemon").src= imgPokemon;
-//document.getElementById("image").appendChild("")
-    } )
-
-
+    referenceMoves = reference + pokemon;
+    getData(reference + pokemon).then(response => {
+        let namePokemon = response.name;
+        let idPokemon = response.id;
+        let imgPokemon = response.sprites.front_default;
+        let moves = response.moves//.slice(0,3);
+        // console.log(namePokemon);
+        // console.log(idPokemon);
+        // console.log("img", imgPokemon);
+       getMoves(moves)
+      //  console.log(response);
+        document.getElementById("namePokemon").innerHTML = namePokemon;
+        document.getElementById("idPokemon").innerHTML = ` id ${idPokemon}`;
+        document.getElementById("imagePokemon").src = imgPokemon;
+        //document.getElementById("movesPokemon").innertml = moves;
+console.log(moves)
+    })
+//console.log(moves)
 }
+// function getMoves(moves){
+//     if (moves.length >=3) {
+//         for (let i = 0; i <= 3; i++) {
+//             console.log(moves[i].move.name)
+//         }
+//     } else {
+//         for (let i = 0; i <= moves.lenght; i++) {
+//             console.log(moves[i].move.name)
+//         }
+//     }
+// }
 
 
+
+
+
+//EVOLUTIONS
+//
+// function getData2 (url){}
+//
+// const result = await fetch (url);
+//let data= await result.json();
+//return data
+//
+// function searchEvolutions(){
+//
+// }
+//
