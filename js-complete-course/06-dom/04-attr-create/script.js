c6c53d19ebcca0fa84b0651671fabadcc0dda51e
Greet Verduyn <greetverduyn@proximus.be>
2022-07-03 20:06:24 +0200

    exercise 6.4 done

--- js-complete-course/06-dom/04-attr-create/script.js
+++ js-complete-course/06-dom/04-attr-create/script.js
@@ -10,7 +10,25 @@
 // You will have time to focus on it later.
 
 (function() {
+//first try
 
-    // your code here
+   /* let section= document.querySelector("section.material>span");
+    console.log(image)*/
+    /*let image3= document.getElementsByTagName('data-image');
+   console.log(image2);*/
 
+    let image2=document.getElementById('source');
+    let url = image2.getAttribute('data-image');
+    console.log(url);
+    console.log(image2);
+
+    let element= document.createElement('img');
+    element.setAttribute('src', url);
+    let figure= document.getElementById('target');
+    figure.appendChild(element);
+
+    let section= document.querySelector("section.material>span");
+       console.log(section)
+
+    section.remove('span')
 })();
