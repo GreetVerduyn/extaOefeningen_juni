eade83606e2903f6d1426cd9461f7eb4949ef20b
Greet Verduyn <greetverduyn@proximus.be>
2022-07-03 12:46:57 +0200

    exercise 11 done

--- js-complete-course/05-arrays/11-rand-array-stats-Optional/script.js
+++ js-complete-course/05-arrays/11-rand-array-stats-Optional/script.js
@@ -11,6 +11,34 @@
 
 (function() {
 
-    // your code here
+    // first try
+
+   /* let numbers= Array(10).fill().map(() => Math.round(Math.random() * 100))
+console.log(numbers)*/  // 0 is also shown
+
+    var numbers = [];
+    while(numbers.length < 10){
+        var r = Math.floor(Math.random() * 100) + 1;
+        if(numbers.indexOf(r) === -1) numbers.push(r);
+    }
+    console.log(numbers)
+
+
+//fill HTML
+
+for (let i=0; i<numbers.length; i++){
+    document.getElementById('n-' + (i+1)).innerHTML=numbers[i];
+}
+
+   // const min = Math.min(...numbers)
+    document.getElementById('min').innerHTML=Math.min(...numbers);
+    document.getElementById('max').innerHTML=Math.max(...numbers);
+
+   /* const som = (number, curr) => number + curr;
+    console.log(numbers.reduce(som));*/
+
+    document.getElementById('sum').innerHTML=numbers.reduce((number1, number2)=> number1+number2, 0);
+    document.getElementById('average').innerHTML=numbers.reduce((number1, number2)=> number1+number2)/numbers.length;
+
 
 })();
